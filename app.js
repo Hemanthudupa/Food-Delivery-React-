634e50c08f209d56194164f0825b9b9f40cb88b8
Hemanthudupa <hemanthudupa06@gmail.com>
2025-03-17 12:16:22 +0530

    add React setup with basic app structure and update .gitignore

---
+++ app.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-delivery-logo-by-abhishek-choudhary-dribbble.jpg" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      {/*
+      this should have 
+    header 
+    body
+    footer  */}
+
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
