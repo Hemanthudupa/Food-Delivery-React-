d35867a78835d8ed20f7c07bfc0ccfe3b7788bf4
Hemanthudupa <hemanthudupa06@gmail.com>
2025-03-19 23:05:22 +0530

    refactor RestroCard and Body components for improved data handling and rendering; update styles in index.css for better layout

--- src/components/RestroCard.jsx
+++ src/components/RestroCard.jsx
@@ -1,13 +1,14 @@
 
-export const RestroCard = ({food:{strMealThumb,strMeal}}) => {
+export const RestroCard = ({food:{strCategoryThumb,strCategory,strCategoryDescription}}) => {
   return (
     <div className="res-card">
-      <img className="res-img" src={strMealThumb} />
+      <img className="res-img" src={strCategoryThumb} />
       <div className="res-card-content">
-        <h3>{strMeal}</h3>
+        <h3>{strCategory}</h3>
+        <p className="content">{strCategoryDescription}</p>
+
         <div className="ratings">
           <h5>{(4 + Math.random()).toFixed(1)}</h5>
-          {/* <p>{deliveryTime}</p> */}
         </div>
       </div>
     </div>
