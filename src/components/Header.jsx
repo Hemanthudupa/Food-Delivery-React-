c08005670dc70bf68a828c26a5d7b2491d2e12c1
Hemanthudupa <hemanthudupa06@gmail.com>
2025-03-20 01:07:44 +0530

    add login/logout button to Header component with state management

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,28 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 export const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={LOGO_URL}
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  let [buttonName, setButtonName] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
       </div>
-    );
-  };
-  
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              setButtonName(buttonName === "Login" ? "Logout" : "Login");
+            }}
+          >
+            {buttonName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
