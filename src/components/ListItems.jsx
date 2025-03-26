f59f836507ca2a0c666c7cce57dad8a18a36cfcf
Hemanthudupa <hemanthudupa06@gmail.com>
2025-03-26 16:11:29 +0530

    feat: enhance ListItems component layout and styling; add 'ADD' button overlay for images

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -10,7 +10,7 @@ const ListItems = ({ data }) => {
         return (
           <div
             key={id}
-            className="flex justify-between items-start bg-white p-4 rounded-lg shadow-md border border-gray-200"
+            className="flex justify-between items-start bg-white p-4 rounded-lg shadow-md  border-gray-100"
           >
             {/* Content */}
             <div className="w-3/4">
@@ -22,12 +22,15 @@ const ListItems = ({ data }) => {
             </div>
 
             {imageId && (
-              <div className="w-24 h-24">
+              <div className="w-24 h-24 relative">
                 <img
-                  src={IMAGE_BASE_URL+""+imageId}
+                  src={IMAGE_BASE_URL + "" + imageId}
                   alt={name}
-                  className="w-full h-full object-cover rounded-lg"
+                  className="w-full h-full object-cover rounded-lg absolute flex justify-center items-center"
                 />
+                <div className="h-auto w-[70%] text-green-500 bg-white rounded-md  flex justify-center relative top-20 left-4 border border-gray-400">
+                  <h1 className="font-sans font-bold">ADD</h1>
+                </div>
               </div>
             )}
           </div>
